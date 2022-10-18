09aa562613bcb05e5027b2aeb126192ce7fb7c8c
perdomo j <joseperdomo2428@gmail.com>
2022-10-18 14:08:05 -0300

    vista principal

--- src/views/PrincipalView.vue
+++ src/views/PrincipalView.vue
@@ -10,27 +10,43 @@
     </ion-header>
     
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">SIRF</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-button expand="block" shape="round" fill="outline" color="green">Block</ion-button>
-      <br/>
-      <ion-button expand="block" shape="round" fill="outline" color="secondary">Block</ion-button>
-      <br/>
-      <ion-button expand="block" shape="round" fill="outline" color="tertiary">Block</ion-button>
-      <br/>
+      <div v-for="item in dataCursos" :key="item.id">
+        <ion-card color="light">
+          <ion-card-header>
+            <ion-card-title>{{item.nombre}}</ion-card-title>
+          </ion-card-header>
+
+          <ion-card-content>
+            Total Estudiantes: {{item.total}}
+          </ion-card-content>
+
+          <ion-button expand="block" shape="round" fill="outline" color="secondary">Pasar Lista</ion-button>
+        </ion-card>
+        <br/>
+      </div>
     </ion-content>
   </ion-page>
    
-  </template>
+</template>
   
-  <script lang="ts">
-    import { IonButton } from '@ionic/vue';
-    import { defineComponent } from 'vue';
-  
+<script lang="ts">
+    import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/vue';
+    import { defineComponent, ref } from 'vue';
+    import { Curso } from "@/interfaces/Curso";
+    
     export default defineComponent({
-      components: { IonButton },
+      components: { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle },
+      setup(){
+        const dataCursos = ref<Curso[]>([{id:123,nombre:"Complejidad Algoritmos",total:43}, {id:456,nombre:"Ing Software",total: 38}]);
+        return { dataCursos }
+      }
     });
-  </script>
+</script>
+
+<style scoped>
+  /* iOS places the subtitle above the title */
+  ion-card-header.ios {
+    display: flex;
+    flex-flow: column-reverse;
+  }
+</style>
